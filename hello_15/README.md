dfcbc01d0ae57895430987362da15d69505a9137
myhat123 <605162579@qq.com>
2020-11-15 15:44:45 +0800

    修改clickhouse建表引擎

--- hello_15/README.md
+++ hello_15/README.md
@@ -20,3 +20,12 @@ go mod init hello_15
 go build -o bin/hello cmd/hello.go
 
 clickhouse-client -u hzg --password 1234
+
+ReplacingMergeTree
+==================
+
+参考资料: http://www.clickhouse.com.cn/topic/5bfccb4953dd87ca52effca7
+
+ClickHouse 在使用 ReplacingMergeTree 进行去重时要注意：  
+1. 只能根据主键去重。
+2. 去重的时候只会去重相同分区的数据，跨分区不会去重，即使是使用 OPTIMIZE **** XXX FINAL 也不会跨分区去重。如果一定要使用这个特性去重，只能将需要去重的数据放在同一个分区。
--- hello_15/sql/schema.sql
+++ hello_15/sql/schema.sql
@@ -27,3 +27,21 @@ ORDER BY (acc, timestamp1);
 --查看分区情况
 
 select * from system.parts where table='brch_qry_dtl';
+
+--表引擎 ReplacingMergeTree 不重复
+--并不能保证进入不重复，需要在合并时才去重
+
+CREATE TABLE finance.brch_qry_dtl (
+    tran_date Date,
+    timestamp1 String,
+    acc String,
+    amt Decimal(10, 2),
+    dr_cr_flag Int,
+    rpt_sum String
+) ENGINE = ReplacingMergeTree
+PARTITION BY toYYYYMM(tran_date)
+ORDER BY (acc, tran_date, timestamp1);
+
+--手动触发合并，以删除重复数据
+
+optimize table brch_qry_dtl;
